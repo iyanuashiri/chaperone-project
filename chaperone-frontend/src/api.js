9e45600b169ae72b364d6240f372ec06b9f3a4e8
Iyanuoluwa Ajao <ajaoiyanu@gmail.com>
2025-05-18 12:30:13 +0100

    refactor: setup authorization header and accesstoken

--- chaperone-frontend/src/api.js
+++ chaperone-frontend/src/api.js
@@ -2,37 +2,36 @@
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/v1/',
+  baseURL: 'http://127.0.0.1:8000/',
   json: true,
 })
 
 export default {
   async execute (method, resource, data) {
-    // let accessToken = localStorage.getItem('authToken')
-    let accessToken = '6f346cef273f199002b9495ae16e8ce6193b145f'
-
+    let accessToken = localStorage.getItem('accessToken')
     return client({
       method,
       url: resource,
       data,
       headers: {
-        Authorization: `Token ${accessToken}`
+        Authorization: `Bearer ${accessToken}`
       }
     }).then(req => {
       return req.data
     })
   },
-  createAssociation(data) {
-    return this.execute('post', '/associations/', data)
-  },
+  
   getAssociations() {
     return this.execute('get', '/associations/')
   },
   getAssociation(id){
     return this.execute('get', `/associations/${id}/`)
   },
-  updateAssociation(id, data) {
-    return this.execute('put', `/associations/${id}/`, data)
+  updateCorrectAssociation(id) {
+    return this.execute('put', `/associations/${id}/correct/`)
+  },
+  updateIncorrectAssociation(id) {
+    return this.execute('put', `/associations/${id}/incorrect/`)
   },
 
 }
