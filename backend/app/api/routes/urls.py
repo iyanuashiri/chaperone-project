1818903eb5af8bdc10336a03b7bff22caf762093
Iyanuoluwa Ajao <ajaoiyanu@gmail.com>
2025-06-25 16:13:04 +0100

    feat: add endpoints to delete url and get list of urls

--- backend/app/api/routes/urls.py
+++ backend/app/api/routes/urls.py
@@ -20,3 +20,28 @@ async def create_url(url: schemas.URLCreate, session: SessionDep, current_user: 
     session.commit()
     session.refresh(db_url)
     return db_url
+
+
+@router.get("/", status_code=status.HTTP_200_OK, response_model=list[schemas.URLRead])
+async def get_urls(session: SessionDep, current_user: models.User = Depends(manager)) -> list[schemas.URLRead]:
+    if not current_user:
+        raise HTTPException(status_code=401, detail="Not authenticated")
+    if not current_user.is_active:
+        raise HTTPException(status_code=403, detail="Inactive user")
+    urls = session.query(models.URL).filter(models.URL.user_id == current_user.id).all()
+    return urls
+
+
+@router.delete("/{url_id}/", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_url(url_id: int, session: SessionDep, current_user: models.User = Depends(manager)):
+    if not current_user:
+        raise HTTPException(status_code=401, detail="Not authenticated")
+    if not current_user.is_active:
+        raise HTTPException(status_code=403, detail="Inactive user")
+
+    url = session.get(models.URL, url_id)
+    if not url:
+        raise HTTPException(status_code=404, detail="URL not found")
+    session.delete(url)
+    session.commit()
+    return
