b2c83bdd36906243bfa28dbe0299bd602fa6f2b8
Iyanuoluwa Ajao <ajaoiyanu@gmail.com>
2025-06-25 15:52:12 +0100

    update alembic

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -2,9 +2,7 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from sqlmodel import SQLModel     # NEW
-
-from app.models import User, Vocabulary, Association, Option   # NEW
+from app.models import Base, User, Vocabulary, URL, Association, Option 
 
 from alembic import context
 
@@ -19,9 +17,9 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from app.models import Base
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-# target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
