034a24f2377cb3bcfcfc363cd3417efe5c8bfbd0
Iyanuoluwa Ajao <ajaoiyanu@gmail.com>
2025-10-30 15:49:19 +0100

    add NewUrl component

---
+++ frontend/src/components/NewUrl.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="bg-white shadow-lg rounded-lg overflow-hidden transform hover:scale-105 transition-transform duration-300 ease-in-out">
+    <router-link :to="{ name: 'url-create' }">
+        <div class="p-6">
+            <h3 class="text-2xl font-semibold text-gray-800 mb-2">{{ title }}</h3>
+            <p class="text-gray-600 text-sm mb-4">{{ description }}</p>
+       
+        </div>
+    </router-link>    
+  </div>
+</template>
+  
+<script>
+  export default {
+    name: 'new-url',
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      description: {
+        type: String,
+        required: true
+
+      },
+      url: {
+        type: String,
+        required: true
+      },
+    },
+    
+  };
+</script>
+  
+  
+  
