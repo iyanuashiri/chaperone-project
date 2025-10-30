10dcad4e28e0c0e3cfd5f58263449e60cda8ec99
Iyanuoluwa Ajao <ajaoiyanu@gmail.com>
2025-10-30 15:51:32 +0100

    update UrlCard component

---
+++ frontend/src/components/UrlCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="bg-white shadow-lg rounded-lg overflow-hidden transform hover:scale-105 transition-transform duration-300 ease-in-out">
+    <div class="p-6">
+      <h3 class="text-2xl font-semibold text-gray-800 mb-2">{{ title }}</h3>
+      <p class="text-gray-600 text-sm mb-4">{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+  
+  <script>
+  export default {
+    name: 'URLCard',
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      description: {
+        type: String,
+        required: true
+      },
+    },
+    
+  };
+  </script>
+  
+  
+  
